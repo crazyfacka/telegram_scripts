8828118fd80911c8afd3fc2795721a88c123c71e
crazyfacka <gmail@joagonca.com>
2024-04-06 16:32:29 +0100

    Added support to send a Telegram generic message through an API call

---
+++ info_api_message.py
@@ -0,0 +1,28 @@
+"""Generate a standard informative message from an API request"""
+from flask import Flask, request, abort
+
+import common.telegram_dispatcher as td
+
+app = Flask(__name__)
+
+@app.route('/send', methods=['GET'])
+def send_message():
+    """Send message through telegram from this API endpoint"""
+    subject = request.args['subject']
+    message = request.args['message']
+    print(f'Received arguments: {subject}, {message}')
+
+    f_subject = f"""
+📢 Informational message!
+
+» {subject} «
+"""
+    success, response = td.send_telegram_message(f_subject, message)
+
+    if success:
+        return f'Message sent successfully: {response}'
+
+    abort(500, f'Failed to send message: {response}')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=6666)
